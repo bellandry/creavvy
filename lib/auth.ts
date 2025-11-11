57fad7bdfeb2330fb0dce19f86d854b8fc96012b
bellandry <bellandry.work@gmail.com>
2025-11-11 17:04:31 +0100

    feature: setup oAuth social providers (github, google, discord)

--- lib/auth.ts
+++ lib/auth.ts
@@ -24,5 +24,13 @@ export const auth = betterAuth({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
   },
 });
