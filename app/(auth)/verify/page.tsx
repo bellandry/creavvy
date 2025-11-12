7402daaf94d17d6ce2c8a94f45c89fb2e02c3468
bellandry <bellandry.work@gmail.com>
2025-11-12 18:08:26 +0100

    update: setting up suspense in verify & check-mail pages

--- app/(auth)/verify/page.tsx
+++ app/(auth)/verify/page.tsx
@@ -1,5 +1,6 @@
 import Verify from "@/components/auth/verify";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Vérification du compte | Creavvy",
@@ -11,5 +12,17 @@ export const metadata: Metadata = {
 };
 
 export default function VerifyPage() {
-  return <Verify />;
+  return (
+    <Suspense
+      fallback={
+        <div className="max-w-md mx-auto p-6 space-y-4 text-white">
+          <div className="h-8 bg-primary/20 rounded w-1/2 animate-pulse"></div>
+          <div className="h-6 bg-primary/10 rounded w-full animate-pulse"></div>
+          <div className="h-6 bg-primary/10 rounded w-1/3 animate-pulse"></div>
+        </div>
+      }
+    >
+      <Verify />
+    </Suspense>
+  );
 }
